4a338ae7afa1deba906cc4352216561670609271
Markus Hassenstein <m.hassenstein@gmx.de>
2020-11-18 08:08:19 +0100

    weather slider

--- index.php
+++ index.php
@@ -1,39 +1,159 @@
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nicht so tolle Wetter App</title>
 
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <div class="hero is-fullheight is-dark is-bold">
-      <div class="container has-text-centered">
-        <div class="content is-small">
-          Hier der Ort und das Datum
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nicht so tolle Wetter App</title>
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <div class="hero is-fullheight is-dark is-bold">
+
+            <div class="hero-body">
+                <div class="container has-text-centered">
+
+
+                    <div class="content is-small">
+                        <span> Berlin,
+                        </span>
+                        <time datetime="1605675993"> Datum von heute
+                        </time>
+                    </div>
+                    <div class="rel state">
+                        <img src='img/hc.svg' alt="Wetterzustand" width="200" title="Wetterzustand">
+                        <p>
+                            <span class="min-temp"> 9° C
+                            </span>
+                            <span class="max-temp"> 9° C
+                            </span>
+                        </p>
+                    </div>
+
+                    <div class="content is-large state">
+                        <p class="is-size-1 is-uppercase has-text-weight-bold"> Hier die Tagline
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
 
-      <div class="rel state">
-        <img src="" alt="Wetter Icon" width="200" height="200">
-        <p>
-          <span class="min-temp">
-            Hier die min. Temperatur
-          </span>
+        <!--Slider -->
 
-          <span class="max-temp">
-            Hier die Max Temperatur
-          </span>
-        </p>
-        </div>
+        <section class="section">
+            <div class="container is-max-desktop">
+                <div id="wetter-slider" class="keen-slider">
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/c.svg" alt="">
+                        </div>
 
-        <div class="content is-large state">
-          <p class="is-size-1 is-uppercase has-text-weight-bold">
-            Hier die Tagline
-          </p>
-        </div>
-      </div>
-      </div>
-    </div>
-  </body>
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/h.svg" alt="">
+                        </div>
+
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/lc.svg" alt="">
+                        </div>
+
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/t.svg" alt="">
+                        </div>
+
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/hc.svg" alt="">
+                        </div>
+
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+                    <div class="keen-slider__slide number-slide">
+                        <h2 class="title">Wochentag</h2>
+                        <div class="tile-img">
+                            <img src="img/c.svg" alt="">
+                        </div>
+
+                        <p>
+                            <span class="min-temp"> 9 C
+                            </span>
+                            <span class="max-temp"> 9 C
+                            </span>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <!-- JS -->
+        <script src="js/keenslider.min.js"></script>
+        <script>
+        var slider = new KeenSlider("#wetter-slider", {
+            spacing: 10,
+            slidesPerView: 1,
+            centered: false,
+            loop: false,
+            mode: "free-snap",
+            dragSpeed: 1,
+            breakpoints: {
+                "(min-width: 768px)": {
+                    slidesPerView: 2,
+                    mode: "free-snap",
+                },
+                "(min-width: 1200px)": {
+                    slidesPerView: 3,
+                    mode: "free-snap",
+                },
+            },
+        })
+        </script>
+
+        <!-- Slider Ende-->
+
+    </body>
+
 </html>
